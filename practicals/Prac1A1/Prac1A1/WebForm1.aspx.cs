d15088c9225f1d9a56ca0a5e2062cd26fe0b0d3c
SANJU20-UI <sanjuuuuuuu2001@gmail.com>
2024-08-22 13:47:24 +0530

    Add files via upload

---
+++ practicals/Prac1A1/Prac1A1/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prac1A1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+         protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(TextBox1.Text);
+            int b = int.Parse(TextBox2.Text);
+            int c = int.Parse(TextBox3.Text);
+            int d = int.Parse(TextBox4.Text);
+
+
+            int product = a * b * c * d;
+            Label2.Text = "product of 4 int no is := " + product;
+        }
+    }
+}
